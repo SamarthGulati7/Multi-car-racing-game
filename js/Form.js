91e5417149ba1b40706954349a8b599bc4a88680
SamarthGulati7 <73104617+SamarthGulati7@users.noreply.github.com>
2021-01-18 14:24:22 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,54 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h1');
+    this.reset = createButton("Reset the data");
+    
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+    this.title.style('color','red');
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+2
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+      this.greeting.style('color','red');
+    });
+    this.reset.position(displayWidth-200,50);
+    this.reset.mousePressed(()=>{
+
+    game.update(0);
+    player.updateCount(0);
+    database.ref('/').update({
+
+    players:null,
+    finishedPlayers:0
+
+    })
+
+    })
+
+  }
+  
+}
